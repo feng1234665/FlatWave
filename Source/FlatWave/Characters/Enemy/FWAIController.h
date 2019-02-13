824bfba2d72db1ffb31601d22741c6d8c3ff5c02
Jan-Niklas Keck <janniklaskeck@online.de>
2019-02-14 00:27:07 +0100

    Add AI Controller class

---
+++ Source/FlatWave/Characters/Enemy/FWAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "FWAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLATWAVE_API AFWAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
