fd437220b5421e29707e6da757b56850947c820e
Jan-Niklas Keck <janniklaskeck@online.de>
2019-02-14 00:28:22 +0100

    Update project files

--- Source/FlatWave/FlatWave.Build.cs
+++ Source/FlatWave/FlatWave.Build.cs
@@ -8,7 +8,7 @@ public class FlatWave : ModuleRules
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Networking", "Sockets" });
 
         PrivateIncludePaths.AddRange(new string[]
         {
