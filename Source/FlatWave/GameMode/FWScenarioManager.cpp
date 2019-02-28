c7ff3c1a97dc71fcec53e9bbd1c85f44c80f2036
Jan-Niklas Keck <janniklaskeck@online.de>
2019-02-28 19:04:15 +0100

    Add InfinityBlade effects

---
+++ Source/FlatWave/GameMode/FWScenarioManager.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FWScenarioManager.h"
+
+// Sets default values
+AFWScenarioManager::AFWScenarioManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFWScenarioManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFWScenarioManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
